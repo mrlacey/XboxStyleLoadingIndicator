ac420f43dfdd1df3df02c4d38d06dbf426f6d6a5
Matt Lacey <github@mrlacey.co.uk>
2015-08-07 15:55:50 +0100

    Example loading indicator

---
+++ XboxLoading/XboxLoading.Shared/XboxStyleLoadingIndicator.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace XboxLoading
+{
+    public sealed partial class XboxStyleLoadingIndicator : UserControl
+    {
+        private bool play = false;
+        private bool running = false;
+
+        public XboxStyleLoadingIndicator()
+        {
+            this.InitializeComponent();
+            (this.Content as FrameworkElement).DataContext = this;
+        }
+
+        public new Brush Foreground
+        {
+            get { return (Brush)GetValue(ForegroundProperty); }
+            set { SetValue(ForegroundProperty, value); }
+        }
+
+        public new static readonly DependencyProperty ForegroundProperty =
+            DependencyProperty.Register("Foreground", typeof(Brush), typeof(XboxStyleLoadingIndicator), new PropertyMetadata(new SolidColorBrush(Windows.UI.Colors.White)));
+
+
+        public new Brush Background
+        {
+            get { return (Brush)GetValue(BackgroundProperty); }
+            set { SetValue(BackgroundProperty, value); }
+        }
+
+        public new static readonly DependencyProperty BackgroundProperty =
+            DependencyProperty.Register("Background", typeof(Brush), typeof(XboxStyleLoadingIndicator), new PropertyMetadata(new SolidColorBrush(Windows.UI.Colors.Green)));
+
+        public new bool IsEnabled
+        {
+            get { return (bool)GetValue(IsEnabledProperty); }
+            set { SetValue(IsEnabledProperty, value); }
+        }
+
+        public new static readonly DependencyProperty IsEnabledProperty =
+            DependencyProperty.Register("IsEnabled", typeof(bool), typeof(XboxStyleLoadingIndicator), new PropertyMetadata(false, OnIsEnabledChanged));
+
+        private static void OnIsEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            XboxStyleLoadingIndicator indicator = d as XboxStyleLoadingIndicator;
+            if (indicator != null)
+            {
+                indicator.IsEnabledChanged();
+            }
+        }
+
+        private new void IsEnabledChanged()
+        {
+            if (this.IsEnabled)
+            {
+                this.Start();
+            }
+            else
+            {
+                this.Stop();
+            }
+        }
+
+        public void Start()
+        {
+            play = true;
+            StartAnimation();
+        }
+
+        public void Stop()
+        {
+            // Don't stop immediately, just let the current storyboard finish
+            play = false;
+        }
+
+        private void StartAnimation()
+        {
+            if (!running)
+            {
+                running = true;
+                this.ShowActivity.Begin();
+            }
+        }
+
+        private void StoryboardCompleted(object sender, object e)
+        {
+            running = false;
+
+            if (play)
+            {
+                this.StartAnimation();
+            }
+        }
+    }
+}
---
+++ XboxLoading/XboxLoading.Windows/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace XboxLoading
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        private bool enabledField;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void StartClicked(object sender, RoutedEventArgs e)
+        {
+            this.Indicator.Start();
+        }
+
+        private void StopClicked(object sender, RoutedEventArgs e)
+        {
+            this.Indicator.Stop();
+
+        }
+
+        public bool Enabled { get {
+                return this.enabledField;
+            } set {
+                this.enabledField = value;
+                RaisePropertyChanged("Enabled");
+            } }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void ToggleClicked(object sender, RoutedEventArgs e)
+        {
+            this.Enabled = !this.Enabled;
+        }
+    }
+}
